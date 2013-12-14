d6518fe0c664ee7bc5c3c53686e3259d907d8420
Mick Brooks <mick.brooks@sinking.in>
2013-12-14 20:04:03 +0000

    Refactor step definitions

--- features/step_definitions/file_ages.rb
+++ features/step_definitions/file_ages.rb
@@ -3,15 +3,28 @@ def later_than(time)
   time + a_bit
 end
 
-def update_mtime(filename, new_mtime)
-  File.utime(File.atime(filename), new_mtime, filename)
+def update_mtime(filenames, new_mtime)
+  [*filenames].each do |f|
+    File.utime(File.atime(f), new_mtime, f)
+  end
+end
+
+def ensure_files_exist(files)
+  files.each do |file|
+    write_file(file, '') unless File.file?(file)
+  end
+end
+
+def latest_mtime_of(files)
+  files.map { |f| File.mtime(f) }.max
 end
 
 def enforce_mtime_order(first, second)
-  write_file(first, '')
-  write_file(second, '')
+  first_files = [*first]
+  second_files = [*second]
+  ensure_files_exist(first_files + second_files)
   in_current_dir do
-    update_mtime(second, later_than(File.mtime(first)))
+    update_mtime(second_files, later_than(latest_mtime_of(first_files)))
   end
 end
 
@@ -24,8 +37,7 @@ Given(/^file "(.*?)" is modified after file "(.*?)"$/) do |second, first|
 end
 
 Given(/^a directory named "(.*?)" with no file modified after file "(.*?)"$/) do |directory, last|
-  files = ['top-level', 'sub-dir/nested']
-  files.map { |file| "#{directory}/#{file}" }.each do |f|
-    enforce_mtime_order(f, last)
-  end
+  files = ['top-level', File.join('sub-dir', 'nested')]
+  paths = files.map { |file| File.join(directory, file) }
+  enforce_mtime_order(paths, last)
 end
