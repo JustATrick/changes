6dfb9ec8f3788bec9d8102f62b0a594de0e5068b
Mick Brooks <mick.brooks@sinking.in>
2013-12-20 15:34:31 +0000

    Start with a failing spec for the mtime_utils/create_file method

---
+++ features/support/spec/mtime_utils_spec.rb
@@ -0,0 +1,27 @@
+require 'mtime_utils'
+
+TMP_DIR = 'tmp/spec'
+
+describe "create_with_mtime" do
+  before(:each) do
+    FileUtils.rm_rf(TMP_DIR)
+    FileUtils.mkdir_p(TMP_DIR)
+    FileUtils.cd(TMP_DIR)
+  end
+
+  context "creating a directory" do
+    it "sets the directory's mtime" do
+      time = Time.new(2013)
+      create_with_mtime('a/c', time)
+      expect(File.mtime('a')).to eq(time)
+    end
+  end
+
+  context "adding a file to an existing directory" do
+    it "sets the directory's mtime when the new file's mtime is older " +
+       "than the directory's"
+
+    it "doesn't change the directory's mtime when the new file's " +
+       "mtime is earlier than the directory's"
+  end
+end
