52a00ac45381078769f16f1332f60744e6450e5e
Mick Brooks <mick.brooks@sinking.in>
2013-12-27 12:31:12 +0000

    Can now remove the 'set the mtime on all directories' hack. The flickering scenarios should not return.

--- features/step_definitions/file_ages.rb
+++ features/step_definitions/file_ages.rb
@@ -21,11 +21,9 @@ end
 
 Given(/^a directory named "(.*?)" with no file modified after file "(.*?)"$/) do |directory, last|
   files = ['top-level', File.join('sub-dir', 'nested')]
-  created_directories = [directory, File.join(directory, 'sub-dir')]
   paths = files.map { |file| File.join(directory, file) }
   in_current_dir do
     ensure_files_exist(last)
     create_with_mtime(paths, earlier_than(mtime_of(last)))
-    update_mtime(created_directories, earlier_than(mtime_of(last)))
   end
 end
